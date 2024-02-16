4ac8e8731845d123300f29b6adc2d15b073551cb
maia <matteusmaia889@gmail.com>
2024-02-16 17:56:43 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
