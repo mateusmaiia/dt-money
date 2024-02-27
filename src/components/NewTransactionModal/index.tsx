dbcc2720db5eaff27601ceed38b8a5ea724eece7
maia <matteusmaia889@gmail.com>
2024-02-27 17:37:27 -0300

    terminando de adicionar o react-hook-form e o zod ao componente NewTransactionModal e adicionando a estilização caso ele esteja disabled

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,7 +5,7 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-const NewTransactionFormSchema = z.object(){
+const NewTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.enum(["income", "outcome"]),
@@ -14,10 +14,15 @@ const NewTransactionFormSchema = z.object(){
 type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>;
 
 export function NewTransactionModal() {
-  const { register, handleSubmit } = useForm<NewTransactionFormInputs>({
+  const { register, handleSubmit, formState: {isSubmitting} } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(NewTransactionFormSchema),
   });
 
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs){
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
   return (
     <Dialog.Portal>
       {/**Faz com que o elemento que está dentro dele vá parar em outro local */}
@@ -26,7 +31,7 @@ export function NewTransactionModal() {
         <S.Content>
           <Dialog.Title>Nova transação</Dialog.Title>
 
-          <form >
+          <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
             <input 
               type="text" 
               placeholder="Descrição"
@@ -57,7 +62,7 @@ export function NewTransactionModal() {
               </S.TransactionTypeButton>
             </S.TransactionType>
 
-            <button type="submit">Cadastrar</button>
+            <button type="submit" disabled={isSubmitting}>Cadastrar</button>
           </form>
 
           <S.Close>
