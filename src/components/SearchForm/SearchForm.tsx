13cd9ec2ac2fe6581d7df78df8cbbc51ef8ac88f
Maia <matteusmaia889@gmail.com>
2024-02-27 02:17:22 -0300

    tipei os inputs do rhf e instalei a dependencia @hookform/resolvers

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -3,12 +3,13 @@ import * as S from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 export function SearchForm() {
-  const { register, handleSubmit } = useForm();
-
   const SearchFormSchema = z.object({
     query: z.string(),
   });
 
+  type SearchFormInputs = z.infer<typeof SearchFormSchema>;
+
+  const { register, handleSubmit } = useForm<SearchFormInputs>();
   function handleSearchTransactions() {
     return;
   }
