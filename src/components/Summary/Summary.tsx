db7e7a60842fdc63bc21ca64ce1bd08c7ecf2ae6
Maia <matteusmaia889@gmail.com>
2024-02-27 01:45:49 -0300

    formatei os valores do componente Summary

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -2,6 +2,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import * as S from "./styles";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
@@ -31,7 +32,7 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </S.SummaryCard>
       <S.SummaryCard>
         <header>
@@ -39,7 +40,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </S.SummaryCard>
       <S.SummaryCard variant="green">
         <header>
@@ -47,7 +48,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </S.SummaryCard>
     </S.SummaryContainer>
   );
