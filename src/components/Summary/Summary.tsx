05d44dfa889060ad331386742c0204295d2b8860
Maia <matteusmaia889@gmail.com>
2024-02-27 01:25:24 -0300

    adicionei os valores do context ao componente Summary e utilizei o reduce para adicionar certinho

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,6 +1,29 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import * as S from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
   return (
     <S.SummaryContainer>
       <S.SummaryCard>
@@ -8,7 +31,7 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.488, 00</strong>
+        <strong>{summary.income}</strong>
       </S.SummaryCard>
       <S.SummaryCard>
         <header>
@@ -16,7 +39,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.488, 00</strong>
+        <strong>{summary.outcome}</strong>
       </S.SummaryCard>
       <S.SummaryCard variant="green">
         <header>
@@ -24,7 +47,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.488, 00</strong>
+        <strong>{summary.total}</strong>
       </S.SummaryCard>
     </S.SummaryContainer>
   );
