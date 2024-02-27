b4353f049122c27cd91c5b78ba47c381b2aa670a
Maia <matteusmaia889@gmail.com>
2024-02-27 01:55:23 -0300

    criei uma pasta 'hooks' e criei o hook useSummary.ts

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,30 +1,8 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import * as S from "./styles";
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useSummary } from "../../hooks/useSummary";}
 import { priceFormatter } from "../../utils/formatter";
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
-
   return (
     <S.SummaryContainer>
       <S.SummaryCard>
