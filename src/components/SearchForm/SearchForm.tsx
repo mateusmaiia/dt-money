2b306a07fc857144e344eb25fe76fc117e49bf5f
Maia <matteusmaia889@gmail.com>
2024-02-27 02:13:53 -0300

    adicionando rhf e zod

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,10 +1,24 @@
 import { MagnifyingGlass } from "phosphor-react";
 import * as S from "./styles";
-
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 export function SearchForm() {
+  const { register, handleSubmit } = useForm();
+
+  const SearchFormSchema = z.object({
+    query: z.string(),
+  });
+
+  function handleSearchTransactions() {
+    return;
+  }
   return (
-    <S.SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+    <S.SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register("query")}
+      />
       <button>
         <MagnifyingGlass size={20} />
         Buscar
